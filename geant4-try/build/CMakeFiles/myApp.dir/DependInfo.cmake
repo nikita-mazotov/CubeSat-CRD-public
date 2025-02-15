f85de3a8872cdf3bbf15a56aafca89a59d427e5b
Nikita <mazotov.nikit@gmail.com>
2025-02-15 14:27:02 -0500

    Changes: in detector construction, added material properties; in stepping action added tracking logic.

--- geant4-try/build/CMakeFiles/myApp.dir/DependInfo.cmake
+++ geant4-try/build/CMakeFiles/myApp.dir/DependInfo.cmake
@@ -8,11 +8,15 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/andres/cubesat/geant4-try/src/main.cpp" "CMakeFiles/myApp.dir/src/main.cpp.o" "gcc" "CMakeFiles/myApp.dir/src/main.cpp.o.d"
+  "/home/coffee/CubeSat-CRD/geant4-try/src/main.cpp" "CMakeFiles/myApp.dir/src/main.cpp.o" "gcc" "CMakeFiles/myApp.dir/src/main.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
